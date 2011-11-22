3f2deec6dc992301c7f72421953a22357c2a3914
Eugene Wechsler <ewechsler@thumbtack.net>
2011-11-22 12:51:58 +0700

    Added home view Login view design

---
+++ pipture/Pipture/Classes/HomeViewController.h
@@ -0,0 +1,13 @@
+//
+//  HomeViewController.h
+//  Pipture
+//
+//  Created by  on 22.11.11.
+//  Copyright 2011 Thumbtack Technology. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HomeViewController : UIViewController
+
+@end
---
+++ pipture/Pipture/Classes/LoginViewController.h
@@ -0,0 +1,20 @@
+//
+//  LoginView.h
+//  Pipture
+//
+//  Created by Vladimir Kubyshev on 21.11.11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface LoginViewController : UIViewController {
+    
+    IBOutlet UITextField *firstNameLabel;
+    IBOutlet UITextField *lastNameLabel;
+    IBOutlet UITextField *emailLabel;    
+}
+- (IBAction)donePressed:(id)sender;
+
+@end
