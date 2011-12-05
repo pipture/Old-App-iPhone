a2c8d3f71c575e1a7f5a27b27e5f5304fcd72a81
Eugene Wechsler <ewechsler@thumbtack.net>
2011-12-05 14:37:27 +0700

    Fix conflict on merge with model_initial_work

--- pipture/Pipture/Timeslot.m
+++ pipture/Pipture/Timeslot.m
@@ -13,17 +13,52 @@
 @synthesize startTime;
 @synthesize endTime;
 @synthesize title;
-@synthesize screenImageURL;
+@synthesize closupBackground;
+
+
+@synthesize desc;
+@synthesize image;
 
 - (id)init
+
+const NSString*JSON_PARAM_START_TIME = @"StartTime";
+const NSString*JSON_PARAM_END_TIME = @"EndTime";
+const NSString*JSON_PARAM_TITLE = @"Title";
+const NSString*JSON_PARAM_CLOSEUP_BACKGROUND = @"CloseupBackground";
+
+
+-(id)initWithJSON:(NSDictionary*)jsonData
 {
     self = [super init];
     if (self) {
-        // Initialization code here.
-        
+        NSNumber*millisecs = [jsonData objectForKey:JSON_PARAM_START_TIME];
+        self.startTime = [NSDate dateWithTimeIntervalSince1970:[millisecs doubleValue]] ;
+        millisecs = [jsonData objectForKey:JSON_PARAM_END_TIME];
+        self.endTime = [NSDate dateWithTimeIntervalSince1970:[millisecs doubleValue]];
+        self.title = [jsonData objectForKey:JSON_PARAM_TITLE];
+        self.closupBackground = [jsonData objectForKey:JSON_PARAM_CLOSEUP_BACKGROUND];                
+    }
+    return self;
+}
+
+- (id)initWith:(NSString*)_title desc:(NSString*)_desc image:(UIImage*)_image {
+    self = [self init];
+    if (self) {
+        self.title = _title;
+        self.desc = _desc;
+        self.image = _image;
     }
     
     return self;
 }
 
+- (void)dealloc {
+    [startTime release];
+    [endTime release];
+    [title release];
+    [screenImageURL release];
+    [desc release];
+    [image release];
+    [super dealloc];
+}
 @end
