87524c8d00065a9c04c752fb2e60897ea1838874
Eugene Wechsler <ewechsler@thumbtack.net>
2011-12-06 12:46:55 +0700

    Data request error handling

--- pipture/Pipture/Classes/DataRequestError.h
+++ pipture/Pipture/Classes/DataRequestError.h
@@ -8,6 +8,17 @@
 
 #import <Foundation/Foundation.h>
 
+#define DRErrorNoInternet -1
+#define DRErrorTimeout -2
+#define DRErrorOther -3
+#define DRErrorUnknown -100;
+
+
 @interface DataRequestError : NSObject
+
+@property(readonly, nonatomic) NSInteger errorCode;
+@property(readonly, nonatomic) NSError *internalError;
+
+- (id)initWithNSError:(NSError*)error;
 
 @end
--- pipture/Pipture/Classes/DataRequestError.m
+++ pipture/Pipture/Classes/DataRequestError.m
@@ -9,5 +9,39 @@
 #import "DataRequestError.h"
 
 @implementation DataRequestError
+@synthesize errorCode = errorCode_;
+@synthesize internalError = internalError_;
+
+- (void)dealloc {
+    [internalError_ release];
+    [super dealloc];
+}
+
+- (id)initWithNSError:(NSError*)error
+{
+    self = [super init];
+    if (self) {
+        if (error) {
+            internalError_ = [error retain];
+            switch ([error code]) {
+                case NSURLErrorNotConnectedToInternet:
+                    errorCode_ = DRErrorNoInternet;
+                    break;
+                case NSURLErrorTimedOut:
+                    errorCode_ = DRErrorTimeout;
+                    break;            
+                default:
+                    errorCode_ = DRErrorOther;            
+                    break;
+            }
+        }
+        else 
+        {
+            internalError_ = nil;
+            errorCode_ = DRErrorUnknown;
+        }
+    }
+    return self;
+}
 
 @end
