9e6c950144685fdc36d4851ae891878f9e4088b6
Vladimir Kubyshev <vkubyshev@thumbtack.net>
2011-12-06 15:54:58 +0700

    -Added own mail composer -Changed video initialization

---
+++ pipture/Pipture/Classes/MailComposerController.h
@@ -0,0 +1,13 @@
+//
+//  MailComposer.h
+//  Pipture
+//
+//  Created by Vladimir Kubyshev on 06.12.11.
+//  Copyright (c) 2011 Thumbtack Technology Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MailComposer : UIViewController
+
+@end
