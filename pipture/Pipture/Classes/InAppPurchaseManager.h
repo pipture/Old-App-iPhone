ea290b29a550eba7f06931e0200817f5b71f82a8
Vladimir Kubyshev <vkubyshev@thumbtack.net>
2011-12-13 16:35:02 +0700

    Upd

--- pipture/Pipture/Classes/InAppPurchaseManager.h
+++ pipture/Pipture/Classes/InAppPurchaseManager.h
@@ -6,8 +6,21 @@
 //  Copyright (c) 2011 Thumbtack Technology Inc. All rights reserved.
 //
 
+#import <StoreKit/StoreKit.h> 
 
+#define kInAppPurchaseManagerProductsFetchedNotification @"kInAppPurchaseManagerProductsFetchedNotification" 
+#define kInAppPurchaseManagerTransactionSucceededNotification @"kInAppPurchaseManagerTransactionSucceededNotification"
+#define kInAppPurchaseManagerTransactionFailedNotification @"kInAppPurchaseManagerTransactionFailedNotification"
 
-@interface InAppPurchaseManager : NSObject <SKProductsRequestDelegate>
+@interface InAppPurchaseManager : NSObject <SKProductsRequestDelegate, SKPaymentTransactionObserver>
+{
+    SKProduct *creditsProduct;
+    SKProductsRequest *productsRequest;
+}
+
+// public methods
+- (void)loadStore;
+- (BOOL)canMakePurchases;
+- (void)purchaseCredits;
 
 @end
