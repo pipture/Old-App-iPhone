f6446f12f7db574563b51a8ecd595bad2c52d243
Eugene Wechsler <ewechsler@thumbtack.net>
2011-12-15 18:08:59 +0700

    DataRequestManager

---
+++ pipture/Pipture/Classes/ThumbtackFW/DataRequestManager.h
@@ -0,0 +1,13 @@
+//
+//  DataRequestManager.h
+//  Pipture
+//
+//  Created by  on 15.12.11.
+//  Copyright (c) 2011 Thumbtack Technology. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol DataRequestManager <NSObject>
+
+@end
