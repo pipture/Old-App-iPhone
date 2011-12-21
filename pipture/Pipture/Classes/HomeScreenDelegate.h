43f3631c2695fb0fcc20bb6041b3ab18f79dc71b
Vladimir Kubyshev <vkubyshev@thumbtack.net>
2011-12-21 10:38:45 +0700

    -HS Delegate protocol

---
+++ pipture/Pipture/Classes/HomeScreenDelegate.h
@@ -0,0 +1,14 @@
+//
+//  HomeScreenDelegate.h
+//  Pipture
+//
+//  Created by Vladimir Kubyshev on 20.12.11.
+//  Copyright (c) 2011 Thumbtack Technology Inc. All rights reserved.
+//
+
+#ifndef Pipture_HomeScreenDelegate_h
+#define Pipture_HomeScreenDelegate_h
+
+
+
+#endif
