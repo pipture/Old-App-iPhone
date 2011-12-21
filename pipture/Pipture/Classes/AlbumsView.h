9182caba9df6bd00f4220c244143e3816430523f
Vladimir Kubyshev <vkubyshev@thumbtack.net>
2011-12-21 13:12:18 +0700

    Albums page

---
+++ pipture/Pipture/Classes/AlbumsView.h
@@ -0,0 +1,13 @@
+//
+//  AlbumsView.h
+//  Pipture
+//
+//  Created by Vladimir Kubyshev on 21.12.11.
+//  Copyright (c) 2011 Thumbtack Technology Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AlbumsView : UIView
+
+@end
