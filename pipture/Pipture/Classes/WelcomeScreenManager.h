bd68df52a0d31f2d995d98fce4d8ac027d8ac7a6
Vladimir Kubyshev <vkubyshev@thumbtack.net>
2011-12-27 17:48:43 +0700

    -added class

---
+++ pipture/Pipture/Classes/WelcomeScreenManager.h
@@ -0,0 +1,13 @@
+//
+//  WelcomeScreenManager.h
+//  Pipture
+//
+//  Created by Vladimir Kubyshev on 27.12.11.
+//  Copyright (c) 2011 Thumbtack Technology Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WelcomeScreenManager : NSObject
+
+@end
---
+++ pipture/Pipture/Classes/WelcomeScreenManager.m
@@ -0,0 +1,13 @@
+//
+//  WelcomeScreenManager.m
+//  Pipture
+//
+//  Created by Vladimir Kubyshev on 27.12.11.
+//  Copyright (c) 2011 Thumbtack Technology Inc. All rights reserved.
+//
+
+#import "WelcomeScreenManager.h"
+
+@implementation WelcomeScreenManager
+
+@end
