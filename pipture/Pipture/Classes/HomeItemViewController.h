ce0b3e182689ddf813b0dbadb7415370c679fb91
Vladimir Kubyshev <vkubyshev@thumbtack.net>
2011-12-28 17:40:37 +0700

    -added home view

---
+++ pipture/Pipture/Classes/HomeItemViewController.h
@@ -0,0 +1,13 @@
+//
+//  HomeItemViewController.h
+//  Pipture
+//
+//  Created by Vladimir Kubyshev on 28.12.11.
+//  Copyright (c) 2011 Thumbtack Technology Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HomeItemViewController : UIViewController
+
+@end
