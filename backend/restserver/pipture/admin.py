ddb3828f389b1d6e04fd8d7ebfdf2d970da821f2
Vladimir Kubyshev <vkubyshev@thumbtack.net>
2012-02-29 13:05:57 +0700

    Fix episode saving

--- backend/restserver/pipture/admin.py
+++ backend/restserver/pipture/admin.py
@@ -216,7 +216,7 @@ class EpisodesAdmin(DeleteForbidden):
 
     def save_model(self, request, obj, form, change):
         user_tz = request.user.get_profile().timezone
-        obj.DateReleased = from_local_to_utc (user_tz, obj.DateReleased)
+        obj.DateReleased = from_local_to_utc_datetime(user_tz, obj.DateReleased)
         obj.save()
     
     def get_form(self, request, obj=None, **kwargs):
