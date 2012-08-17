cb62e750ecd0bd8814c51cf3d6d7f68d54be128a
Vladimir Kubyshev <crom83@mail.ru>
2012-08-17 13:16:48 +0400

    #16407 - Your channel view Controller wireframe

---
+++ iphone/pipture/Pipture/Classes/CategoryEditViewController.h
@@ -0,0 +1,13 @@
+//
+//  CategoryEditViewController.h
+//  Pipture
+//
+//  Created by Vladimir on 17.08.12.
+//  Copyright (c) 2012 Thumbtack Technology. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CategoryEditViewController : UIViewController
+
+@end
---
+++ iphone/pipture/Pipture/Classes/CategoryEditViewController.m
@@ -0,0 +1,43 @@
+//
+//  CategoryEditViewController.m
+//  Pipture
+//
+//  Created by Vladimir on 17.08.12.
+//  Copyright (c) 2012 Thumbtack Technology. All rights reserved.
+//
+
+#import "CategoryEditViewController.h"
+
+@interface CategoryEditViewController ()
+
+@end
+
+@implementation CategoryEditViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
