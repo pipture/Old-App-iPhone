36285f1ceaefe364365a46a76e3e870a9f423981
Artem Kondratyev <akondratyev@thumbtack.net>
2012-09-03 16:17:49 +0700

    #16905: frontend

--- iphone/pipture/Pipture/Classes/PiptureAPI/Entities/CategoryItem.h
+++ iphone/pipture/Pipture/Classes/PiptureAPI/Entities/CategoryItem.h
@@ -13,6 +13,12 @@
 
 @property(assign, nonatomic) NSInteger id;
 @property(retain, nonatomic) NSString* title;
+@property(retain, nonatomic) NSString* line1;
+@property(retain, nonatomic) NSString* line2;
+@property(retain, nonatomic) NSString* episodeNo;
+@property(retain, nonatomic) NSString* albumTitle;
+@property(retain, nonatomic) NSString* seriesTitle;
+@property(retain, nonatomic) NSString* albumSeason;
 @property(retain, nonatomic) NSString* thumbnail;
 @property(retain, nonatomic) NSString* type;
 @property(retain, nonatomic) PlaylistItem* playlistItem;
